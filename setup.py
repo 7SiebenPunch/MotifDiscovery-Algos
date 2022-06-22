50ffab40ee1c99827651214f26fe0e30f3a22a08
7SiebenPunch <101822242+7SiebenPunch@users.noreply.github.com>
2022-06-22 17:20:52 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+
+from setuptools import setup
+
+setup(
+        name='mdalgos',     
+        version='1.0',   
+        description='Integration of some of the best existing Motif discovery algorithms in time series.',   
+        author='Jiajun Qi',  
+        url='https://github.com/7SiebenPunch/MotifDiscovery-Algos',     
+        packages=find_packages()                
+)
