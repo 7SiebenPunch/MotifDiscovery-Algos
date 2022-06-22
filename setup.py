379e8595bc2925efc55160f68c5fc70e0359969e
7SiebenPunch <101822242+7SiebenPunch@users.noreply.github.com>
2022-06-22 17:39:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
         name='mdalgos',     
         version='1.0',   
         description='Integration of some of the best existing Motif discovery algorithms in time series.',   
-        author='Jiajun Qi',  
+        author='Jiajun Qi',
+        license='MIT',
         url='https://github.com/7SiebenPunch/MotifDiscovery-Algos',     
         packages=['mdalgos']                
 )
